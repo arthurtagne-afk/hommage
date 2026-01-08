0555757e7b137040edc2095c09b879d2a0d77664
arthurtagne-afk <arthurtagne@mediaintelligence.fr>
2026-01-08 08:37:18 +0100

    Create textData.js

---
+++ textData.js
@@ -0,0 +1,170 @@
+anecdotes = [
+    {
+        "auteur": "Sa Majesté Fouo Ngopong Fofou Dieudonée",
+        "anecdote": "En 1958, pendant le maquis, Papa Jean a hébergé ma maman pendant 3 mois à l'hôpital UICEM de Bouddha, alors que j'étais gravement malade."
+    },
+    {
+        "auteur": "Sa Majesté Fouo Ngopong Fofou Dieudonée",
+        "anecdote": "Papa Jean m'amenait toujours au marché pour vendre des animaux (coqs, porcs, chèvres) - c'est ainsi que je suis devenu commerçant."
+    },
+    {
+        "auteur": "Sa Majesté Fouo Ngopong Fofou Dieudonée",
+        "anecdote": "Papa Jean offrait systématiquement un régime de plantain et un morceau de bois à chacune de mes épouses lors de leur mariage - tradition qu'il a respectée pour toutes mes femmes."
+    },
+    {
+        "auteur": "Sa Majesté Fouo Ngopong Fofou Dieudonée",
+        "anecdote": "Quand j'hésitais à acheter mon terrain (prix élevé), Papa Jean m'a encouragé et convaincu d'aller à Debreuil payer - ce terrain est devenu ma concession."
+    },
+    {
+        "auteur": "Sa Majesté Fouo Ngopong Fofou Dieudonée",
+        "anecdote": "Papa Jean a servi de témoin des DEUX côtés lors de l'acquisition de l'entrée de ma concession - preuve de la confiance absolue qu'on lui portait."
+    },
+    {
+        "auteur": "Sa Majesté Fouo Ngopong Fofou Dieudonée",
+        "anecdote": "Après la saisie de nos marchandises par la douane au Nigéria, Papa Jean s'est reconverti courageusement dans le commerce de chèvres vers Yaoundé et Douala."
+    },
+    {
+        "auteur": "Tedondzong Paul, Petit Frère",
+        "anecdote": "Papa Jean était l'un des tout premiers Batchams à faire le commerce de ballots de tissus depuis le Nigeria dans les années 60, surnommé 'Brelerou' (Albinos) par les douaniers qui le laissaient toujours passer."
+    },
+    {
+        "auteur": "Tedondzong Paul, Petit Frère",
+        "anecdote": "Après des années de succès au Nigeria, Papa Jean a tout perdu quand un douanier a saisi toutes ses marchandises - fin brutale d'une route où il était pourtant très respecté."
+    },
+    {
+        "auteur": "Tedondzong Paul, Petit Frère",
+        "anecdote": "Papa Jean avait négocié une licence exclusive de distribution de boisson dans le Bamboutos, mais un homme politique l'a détournée et exploitée à sa place - sans éducation formelle, il n'a pas pu se défendre."
+    },
+    {
+        "auteur": "Tedondzong Paul, Petit Frère",
+        "anecdote": "Après l'échec de la licence, Papa Jean s'est réinventé comme représentant des planteurs à la coopérative UCCAO, collectant leurs productions pour les ramener à Boudoua."
+    },
+    {
+        "auteur": "Tedondzong Paul, Petit Frère",
+        "anecdote": "Entre ses différentes activités, Papa Jean achetait des chèvres et des porcs qu'il allait vendre à Douala et Yaoundé - un homme qui a tout essayé pour réussir."
+    },
+    {
+        "auteur": "Tedondzong Paul, Petit Frère",
+        "anecdote": "Malgré tous ses échecs commerciaux et sa situation démunie en fin de vie, Papa Jean n'a jamais perdu ni sa dignité, ni son humanité, ni son amour pour sa famille."
+    },
+    {
+        "auteur": "Adjudant de Gendarmerie Ngoufo Moïse Gilbert",
+        "anecdote": "Présenté par Fouomouo Lucas, j'ai développé avec Papa Jean une relation plus profonde que Lucas lui-même - une fraternité dépassant l'amitié initiale."
+    },
+    {
+        "auteur": "Adjudant de Gendarmerie Ngoufo Moïse Gilbert",
+        "anecdote": "À chaque retour au village, je voyais Papa Jean EN PREMIER - impossible de prendre une décision sans d'abord m'asseoir avec lui et débattre jusqu'à trouver un accord."
+    },
+    {
+        "auteur": "Adjudant de Gendarmerie Ngoufo Moïse Gilbert",
+        "anecdote": "Depuis mon poste de gendarme ailleurs, j'envoyais des lettres avec instructions et argent à Papa Jean pour régler des affaires au village - exécution toujours impeccable, confiance totale."
+    },
+    {
+        "auteur": "Adjudant de Gendarmerie Ngoufo Moïse Gilbert",
+        "anecdote": "Je craignais que ma proximité exceptionnelle avec Papa Jean (plus forte qu'avec son propre frère) ne crée des jalousies dans ma propre famille."
+    },
+    {
+        "auteur": "Adjudant de Gendarmerie Ngoufo Moïse Gilbert",
+        "anecdote": "Papa Jean est mort brutalement dans un accident de voiture - amené à l'hôpital, il a rendu l'âme directement à l'arrivée, sans que je ne puisse lui dire au revoir."
+    },
+    {
+        "auteur": "Adjudant de Gendarmerie Ngoufo Moïse Gilbert",
+        "anecdote": "Je ne cachais RIEN à Papa Jean - toutes mes affaires personnelles, tous mes projets - une transparence totale qui témoigne d'une confiance jamais trahie."
+    },
+    {
+        "auteur": "Mafopa Tchinda Ruth Épouse Diffo",
+        "anecdote": "J'ai rencontré Papa Jean à 5 ans après la mort de mon père - même humour, même voix, même passion pour la cigarette : j'ai retrouvé mon père en lui."
+    },
+    {
+        "auteur": "Mafopa Tchinda Ruth Épouse Diffo",
+        "anecdote": "Papa Jean m'a appris mes premiers mots en langue maternelle - il m'a aidée à apaiser la douleur de l'absence paternelle."
+    },
+    {
+        "auteur": "Mafopa Tchinda Ruth Épouse Diffo",
+        "anecdote": "Papa Jean m'emmenait partout, même dans des lieux 'réservés aux hommes' - à ceux qui questionnaient, il répondait : 'Qui a décidé que c'est réservé aux hommes ?'"
+    },
+    {
+        "auteur": "Mafopa Tchinda Ruth Épouse Diffo",
+        "anecdote": "Je me sentais unique et chanceuse dans ces cercles masculins - cette expérience a forgé mon abnégation et ma résilience."
+    },
+    {
+        "auteur": "Mafopa Tchinda Ruth Épouse Diffo",
+        "anecdote": "J'avais promis d'emmener Papa Jean voyager au-delà des mers - il est parti trop tôt, avant que je ne puisse tenir ma promesse."
+    },
+    {
+        "auteur": "Mafopa Tchinda Ruth Épouse Diffo",
+        "anecdote": "Le jour de mon mariage, j'étais meurtrie - perdre Papa Jean m'avait rendue orpheline pour la deuxième fois de ma vie."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Papa Jean a sauvé mon petit frère Tedondzong Paul gravement malade après la mort de notre mère - il l'a emmené à Mbouda voir un guérisseur au marché de tapioca, puis l'a envoyé à l'école."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Papa Jean a aussi pris en charge Tchinda André, l'envoyant à l'école - celui-ci est plus tard entré dans l'armée grâce à son soutien."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Quand mon petit frère a guéri, Papa Jean a organisé le 'boum' (fête de mariage) dans SA propre maison au toit de paille, pas chez notre père - il s'était substitué au rôle paternel."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Papa Jean aidait aussi ses frères consanguins (d'autres maisons) - payer les dots, financer l'école - aucun ne peut nier son intervention dans leur vie."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Quand notre père était malade, Papa Jean était le SEUL à rester à son chevet à l'hôpital du début à la fin - présence indéfectible."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Notre père a confié à Papa Jean son habit de samba pour le représenter partout et l'envoyait personnellement sur les lieux de sacrifice - confiance totale."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Pour tous les enfants adoptés dans notre maison familiale, Papa Jean était systématiquement la personne de confiance désignée par notre père."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Papa André a confié son premier fils Lontsi Emmanuel (homonyme du père) à Papa Jean de son vivant - puis TOUS ses autres enfants après sa mort."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "À chaque naissance, Papa André venait voir Papa Jean pour que CE DERNIER choisisse le nom de l'enfant - André n'avait rien à dire, il confiait tout à son grand frère."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Papa Jean et Papa André étaient 'comme une seule personne' - ils ne se séparaient de rien, fusion fraternelle totale."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Tchinda André a envoyé mon fils au collège à Batcham ville, prenant en charge son éducation complète."
+    },
+    {
+        "auteur": "Maffopa Madeleine",
+        "anecdote": "Chaque mois après avoir reçu son salaire à Bafoussam, Tchinda André venait au village me donner huile et savon - rituel maintenu jusqu'à sa mort."
+    },
+    {
+        "auteur": "Lontsi Naderge",
+        "anecdote": "Quand on venait voir Papa Jean avec un problème, il se rendait TOTALEMENT disponible - il s'investissait à 100% comme si c'était sa propre affaire."
+    },
+    {
+        "auteur": "Lontsi Naderge",
+        "anecdote": "Papa Jean ne connaissait pas la colère - il parlait toujours doucement, acceptait tout le monde. Au pire, il haussait les épaules, se retournait et restait tranquille."
+    },
+    {
+        "auteur": "Lontsi Naderge",
+        "anecdote": "Papa Jean a été percuté par une voiture à Boudoua alors qu'il allait donner des bananes à Gilbert pour que celui-ci s'occupe des enfants d'un cousin éloigné."
+    },
+    {
+        "auteur": "Lontsi Naderge",
+        "anecdote": "Quand un cousin éloigné a perdu sa fille et qu'il fallait faire l'autopsie, Papa Jean est allé par-derrière et s'est présenté LUI-MÊME pour le faire à sa place."
+    },
+    {
+        "auteur": "Lontsi Naderge",
+        "anecdote": "Papa Jean avait 'un cœur qu'on ne comprenait pas' - dès qu'un de ses frères avait une difficulté, il prenait automatiquement le devant pour la résoudre."
+    },
+    {
+        "auteur": "Lontsi Naderge",
+        "anecdote": "Malgré les divergences inévitables d'une grande famille, Papa Jean faisait tout pour réunir tout le monde et surmonter les tensions - il accueillait tout le monde."
+    }
+]
